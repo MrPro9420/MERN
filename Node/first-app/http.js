3e3c9227154940472263ca47e5f02b9d2309885b
Prabhat Pal <palprabhat9420@gmail.com>
2026-03-06 15:11:35 +0530

    http module updated

--- Node/first-app/http.js
+++ Node/first-app/http.js
@@ -7,7 +7,7 @@ const server =  http.createServer((req,res)=>{
     }
 
     if(req.url ==="/api/courses"){
-        res.write(JSON.stringify([1,2,3,4]))
+        res.write(JSON.stringify([1,2,3,4,5]))
         res.end();
     }
 });
