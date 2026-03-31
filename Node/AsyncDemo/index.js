d51160e74aabc864eb106d6e1e7a78e3f293095d
Prabhat Pal <palprabhat9420@gmail.com>
2026-03-31 18:01:30 +0530

    async await added

--- Node/AsyncDemo/index.js
+++ Node/AsyncDemo/index.js
@@ -1,22 +1,95 @@
 console.log("Start");
-const user = getUserData(2, (user) => {
-    getRepos(user.name, (repos) => {
-        console.log(repos)
-    })
 
-});
+// Callback hell
+// getUserData(2, (user) => {
+//     getRepos(user.name, (repos) => {
+//         getCommits(repos[0], (repo) => {
+//             console.log(user)
+//             console.log(repo)
+//             console.log("No commit")
+//         })
+//     })
 
-function getUserData(id, callback) {
-    setTimeout(() => {
-        callback({ id: id, name: "Mosh" });
-    }, 2000)
+// });
+
+// Named function for resolving callback hell
+
+
+
+// function displayUserData(user) {
+//     getRepos(user.name, displayRepos);
+// }
+
+// function displayRepos(repos) {
+//     getCommits(repos[0], displayCommits)
+// }
+
+// function displayCommits(repo) {
+
+//     console.log(repo)
+//     console.log("No commit")
+// }
+
+//Promises
+
+// getUserData(2)
+//     .then((user) => getRepos(user.name))
+//     .then(repos => getCommits(repos[0]))
+//     .then(commits => { console.log(commits) })
+//     .catch(err => console.log(err.message))
+
+
+// Async and Await approch
+
+async function displaycommits() {
+
+    try {
+        const user = await getUserData(1);
+        const repos = await getRepos(user.name);
+        const commits = await getCommits(repos[0]);
+        console.log(commits)
+    }
+    catch (err) {
+        console.log(err.message)
+    }
 }
 
-function getRepos(username, callback) {
-    setTimeout(() => {
-        console.log("fetching user data " + username);
-        callback(["repo1", "repo2", "repo3"]);
-    }, 2000)
+displaycommits();
+
+
+
+
+
+
+function getUserData(id) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve({ id: id, name: "Mosh" });
+        }, 2000)
+    })
+
+}
+
+function getRepos(username) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            console.log("fetching user data " + username);
+            // resolve(["repo1", "repo2", "repo3"]);
+            reject(new Error("No data Found"))
+        }, 2000)
+
+    })
+
+}
+
+function getCommits(repo) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            console.log("Fetching commits ...");
+            resolve(repo);
+        }, 3000)
+    })
+
 }
 
 console.log("END")
